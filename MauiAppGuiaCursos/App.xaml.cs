c1d37a8a974e2be7400ddac4670cf82c4876b87f
Usuario <Usuario@CPD03-M16>
2024-09-30 15:25:16 -0300

    .

---
+++ MauiAppGuiaCursos/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiAppGuiaCursos
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
